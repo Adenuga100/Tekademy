951f32639f57b958916c7b8e31a34732cf8ee411
Adenuga100 <adenugaadeyemiisaac@gmail.com>
2026-04-15 10:37:12 +0100

    Forgotpassword

--- pages/loginPage.ts
+++ pages/loginPage.ts
@@ -4,35 +4,61 @@ import { Locator, Page } from "@playwright/test";
 
 export class LoginPage {
     constructor(private page: Page) {}
+       // Locators
+  passwordInput =() => this.page.locator('input[id="password"]');
+  emailInput = () =>  this.page.locator('input[id="email"]');
+  forgotPswclick  = (forgotpsw: string) => this.page.locator(`a:has-text("${forgotpsw}")`);
+  resetButton = (restbtn: string) => this.page.locator(`button:has-text("${restbtn}")`);
+  clickLoginBtn = () =>  this.page.locator('button[type="submit"]');
+  clickRememberMe = () => this.page.locator('input[id="remember"]');
+  homeTitle = () => this.page.locator('img[class="object-contain w-full"]');
+  verified = () => this.page.getByText('Verify Your Email');
+ clickBtn = (btn: string) => this.page.locator(`button:has-text("${btn}")`);
+ errorLocator = (expectedMessage: string) => this.page.getByText(expectedMessage);
 
     async navigate() {
         await this.page.goto('https://tk-academy-admin.vercel.app/auth/login');
     }
 
     async enterPassword(password: string) {
-        return this.page.fill('input[id="password"]', password);
+        return await this.passwordInput().fill(password);
     }
 
     async enterEmail(email: string) {
-        return this.page.fill('input[id="email"]', email);
+        return await this.emailInput().fill(email);
     }
 
+    async forgetPassword(forgotpsw: string) {
+        return await this.forgotPswclick(forgotpsw).click();
+    }
+
+    async resetBtn(restbtn: string) {
+        return await this.resetButton(restbtn).click();
+    }
+
+    
+
     async clickLoginButton() {
-        return await this.page.click('button[type="submit"]');
+        return await this.clickLoginBtn().click();
     }
 
     async clickRememberMeCheckbox() {
-        return await this.page.click('input[id="remember"]');
+        return await this.clickRememberMe().click();
     }
 
     async getHomeTitle(): Promise<Locator> {
-        let homeTitle = await this.page.locator('[class="object-contain w-full"]'); 
-        return homeTitle;
+      
+        return await this.homeTitle();
     }
 
     async errorMessage(expectedMessage: string): Promise<Locator> {
-        let errorLocator = await this.page.getByText(expectedMessage); 
-        return errorLocator;
+       
+        return await this.errorLocator(expectedMessage);
+    }
+
+    async getVerified(): Promise<Locator> {
+       
+        return await this.verified();
     }
 
 }
