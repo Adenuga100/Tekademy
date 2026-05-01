28a26f247e590ae6cdf879cfb4805b28614dcb4b
Adenuga100 <adenugaadeyemiisaac@gmail.com>
2026-05-01 12:28:32 +0100

    Upload resource

--- pages/loginPage.ts
+++ pages/loginPage.ts
@@ -5,34 +5,147 @@ import { Locator, Page } from "@playwright/test";
 export class LoginPage {
     constructor(private page: Page) {}
 
-    async navigate() {
-        await this.page.goto('https://tk-academy-admin.vercel.app/auth/login');
+   // Locators
+  passwordInput =() => this.page.locator('input[id="password"]');
+  emailInput = () =>  this.page.locator('input[id="email"]');
+  clickLoginBtn = () =>  this.page.locator('button[type="submit"]');
+  clickRememberMe = () => this.page.locator('input[id="remember"]');
+  fillTitle = () => this.page.locator('input[id="resource-title"]');
+  clickAddResource = () => this.page.locator('button[type="submit"]');
+  unCheck = () => this.page.locator('input[type="checkbox"]');
+  closeX = () => this.page.locator('button[data-slot="dialog-close"]');
+  fillDescription = () => this.page.locator('textarea[id="resource-description"]');
+  fillTags = () => this.page.locator('input[id="resource-tags"]');
+  fillFileUrl = () => this.page.locator('input[type="url"]');
+  files = () => this.page.locator('input[type="file"]');
+  clickFileUrlAndIsPublisheddFile = () => this.page.getByText('— fileUrl, isPublished');
+  category = () => this.page.locator('//button[@id="resource-category"]/following-sibling::select');
+  homeTitle = () => this.page.locator('img[class="object-contain w-full"]');
+ clickBtn = (btn: string) => this.page.locator(`button:has-text("${btn}")`);
+ errorLocator = (expectedMessage: string) => this.page.getByText(expectedMessage);
+  success = (successmessage: string) => this.page.getByText(successmessage);
+  libraryPages = () => this.page.getByText('Library & Resources');
+  menuButton = (menu: string) => 
+    this.page.locator(`span:has-text("${menu}")`);
+//span[text()="${menu}"]
+ // Drop the 'function' keyword
+    generateRandomName() { 
+        const firstNames = ["Alex", "Jordan", "Taylor", "Casey", "Morgan"];
+        const lastNames = ["Smith", "Johnson", "Reed", "Walker", "Knight"];
+        const years = ["2000", "1991", "2001", "2002", "2003","2005"];
+        const months = ["01", "02", "03", "04", "05","04","05", "06", "07", "08", "09","10","11", "12"];
+        const dates = ["01", "02", "03", "04", "05","04","05", "06", "07", "08", "09","10","11", "12","13", "14", "15", "16", "17","18"];
+
+        const first = firstNames[Math.floor(Math.random() * firstNames.length)];
+        const last = lastNames[Math.floor(Math.random() * lastNames.length)];
+        const year = years[Math.floor(Math.random() * years.length)];
+        const month = months[Math.floor(Math.random() * months.length)];
+        const date = dates[Math.floor(Math.random() * dates.length)];
+
+        return { first, last , year, month,date};
     }
+  // Actions
+  async navigate() {
+    return await this.page.goto('https://tk-academy-admin.vercel.app/auth/login');
+  }
 
     async enterPassword(password: string) {
-        return this.page.fill('input[id="password"]', password);
+       return await this.passwordInput().fill(password);
     }
 
-    async enterEmail(email: string) {
-        return this.page.fill('input[id="email"]', email);
-    }
+  async enterEmail(email: string) {
+    return await this.emailInput().fill(email);
+  }
+
+  async clickMenu(menu: string) {
+   await this.menuButton(menu).scrollIntoViewIfNeeded();
+   return await this.menuButton(menu).click();
+  }
+  
+   async clickButton(btn: string) {
+    await this.clickBtn(btn).scrollIntoViewIfNeeded();
+    return await this.clickBtn(btn).click();
+ }
+
+
 
     async clickLoginButton() {
-        return await this.page.click('button[type="submit"]');
+        return await this.clickLoginBtn().click();
     }
 
     async clickRememberMeCheckbox() {
-        return await this.page.click('input[id="remember"]');
+        return await this.clickRememberMe().click();
     }
 
     async getHomeTitle(): Promise<Locator> {
-        let homeTitle = await this.page.locator('[class="object-contain w-full"]'); 
-        return homeTitle;
+        
+        return await this.homeTitle();
     }
 
     async errorMessage(expectedMessage: string): Promise<Locator> {
-        let errorLocator = await this.page.getByText(expectedMessage); 
-        return errorLocator;
+        await this.errorLocator(expectedMessage).scrollIntoViewIfNeeded();
+        return await this.errorLocator(expectedMessage);
+    }
+
+    async successFulMessage(successmessage: string): Promise<Locator> {
+        
+        return await this.success(successmessage);
+    }
+
+    async libraryPage(): Promise<Locator> {
+        
+        return await this.libraryPages();
+    }
+
+   
+
+    async enterTitle(title: string) {
+        return await this.fillTitle().fill(title);
+    }
+
+    async addResource() {
+        await this.clickAddResource().scrollIntoViewIfNeeded();
+        return await this.clickAddResource().click();
+    }
+
+    async uncheckPublish() {
+        await this.unCheck().scrollIntoViewIfNeeded();
+        return await this.unCheck().click();
+    }
+
+    async close() {
+      
+        return await this.closeX().click();
+    }
+
+
+
+    async selectCategory(category: string) {
+        return await this.category().selectOption({ label: category });
+    }
+
+    async enterDescription(description: string) {
+        await this.fillDescription().scrollIntoViewIfNeeded();
+        return await this.fillDescription().fill(description);
+    }
+
+    async enterFileUrl(fileUrl: string) {
+        await this.fillFileUrl().scrollIntoViewIfNeeded();
+        return await this.fillFileUrl().fill(fileUrl);
+    }
+    async enterTags(description: string) {
+        await this.fillTags().scrollIntoViewIfNeeded();
+        return await this.fillTags().fill(description);
+    }
+
+    async uploadFile(filename: string) {
+        await this.files().scrollIntoViewIfNeeded();
+        return await this.files().setInputFiles(filename);
+    }
+
+    async clickFileUrlAndIsPublished() {
+        await this.clickFileUrlAndIsPublisheddFile().scrollIntoViewIfNeeded();
+        return await this.clickFileUrlAndIsPublisheddFile().click();
     }
 
 }
--- steps/loginStep.ts
+++ steps/loginStep.ts
@@ -30,6 +30,7 @@ When('User click on login button', async () => {
 })
 
 
+
 Then('I should be redirected to the dashboard', async () => {
   // Write code here that turns the phrase above into concrete actions
   await expect( await loginPage.getHomeTitle()).toBeVisible({ timeout: 5000 });
@@ -44,4 +45,88 @@ Then('user receive error message as {string}', async (expectedMessage: string) =
   // Write code here that turns the phrase above into concrete actions
    await expect( await loginPage.errorMessage(expectedMessage)).toHaveText(expectedMessage);
 })
+
+
+
+When('User click on {string} button', async (btn: string) => {
+  // Write code here that turns the phrase above into concrete actions
+ await loginPage.clickButton(btn);
+})
+
+When('User click on {string} menu', async (menus: string) => {
+  await loginPage.clickMenu(menus);
+})
+
+
+
+
+When('User enter invalid email as {string}', async (email: string) => {
+  // Write code here that turns the phrase above into concrete actions
+  await loginPage.enterEmail(email);
+})
+
+Then('User should see a success message {string}', async (successmessage: string) => {
+  // Write code here that turns the phrase above into concrete actions
+  await expect( await loginPage.successFulMessage(successmessage)).toHaveText(successmessage);
+})
+
+
+
+When('User enter tags as {string}', async (tags: string) => {
+  // Write code here that turns the phrase above into concrete actions
+  await loginPage.enterTags(tags);
+})
+
+When('User select category as {string}', async (category: string) => {
+  // Write code here that turns the phrase above into concrete actions
+  await loginPage.selectCategory(category);
+})
+
+When('User upload file {string}', async (filename: string) => {
+  // Write code here that turns the phrase above into concrete actions
+  await loginPage.uploadFile(filename);
+})
+
+When('User click fileUrl and isPublished', async () => {
+  // Write code here that turns the phrase above into concrete actions
+  await loginPage.clickFileUrlAndIsPublished();
+})
+
+When('User enter description as {string}', async  (descrip: string) => {
+  // Write code here that turns the phrase above into concrete actions
+  await loginPage.enterDescription(descrip);
+})
+
+When('User enter fileUrl as {string}', async (fileUrl: string) => {
+  // Write code here that turns the phrase above into concrete actions
+  await loginPage.enterFileUrl(fileUrl);
+})
+
+When('User enter title as {string}', async (title: string) => {
+  // Write code here that turns the phrase above into concrete actions
+  await loginPage.enterTitle(title);
+})
+
+When('User click on Add Resource button', async () => {
+  // Write code here that turns the phrase above into concrete actions
+  await loginPage.addResource();
+})
+
+When('User uncheck the Publish immediately checkbox',async () => {
+  // Write code here that turns the phrase above into concrete actions
+  await loginPage.uncheckPublish();
+})
+
+Then('User should be redirected to library page', async () => {
+  // Write code here that turns the phrase above into concrete actions
+  await expect( await loginPage.libraryPage()).toBeVisible({ timeout: 5000 });
+
+})
+
+When('User click on x button', async () => {
+  // Write code here that turns the phrase above into concrete actions
+  await loginPage.close();
+})
+
+
 
